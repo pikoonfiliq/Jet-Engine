fa2931a8f3c49cd441c4170749b9d7d35bf97f9a
Евгени Димитров Попов <106150787+pikoonfiliq@users.noreply.github.com>
2024-04-14 18:55:54 +0300

    Update Display
    Added initialisation

--- Display.ino
+++ Display.ino
@@ -1,1 +1,23 @@
+#include <LCD-I2C.h>
+
+LCD_I2C lcd(0x27, 20, 4);
+
+void DisplaySetup(){
+  //inniting the LCD
+  lcd.begin();
+  lcd.display();
+  lcd.backlight();
+
+}
+
+void DisplayBeninging(){
+  DisplaySetup();
+  lcd.setCursor(3, 0);
+  lcd.print("Engine testing");
+  lcd.setCursor(7, 1);
+  lcd.print("station");
+  delay(5000);
+  lcd.clear();
+}
+
 
